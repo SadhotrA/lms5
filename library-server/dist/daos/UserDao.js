a619f30073827a246a63e2f440600c8510d31e86
vishal sadhotra <vishalsadhotra00@gmail.com>
2024-12-13 16:20:44 +0530

    bib

---
+++ library-server/dist/daos/UserDao.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+;
+const UserSchema = new Schema({
+    type: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+}, {
+    versionKey: false,
+});
+exports.default = mongoose_1.default.model("User", UserSchema);
